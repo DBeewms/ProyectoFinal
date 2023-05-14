840e5b4afef4504a26bd50a88fc96c967d5d6696
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-13 19:50:10 -0600

    Se agregó la estructura cliente

---
+++ clientes/clientes.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+
+
+typedef struct
+{
+    char ID[14];
+    char nombre[30];
+    char apellido[30];
+    char direccion[100];
+}cliente[MAX];
+
+cliente clientes[MAX];
