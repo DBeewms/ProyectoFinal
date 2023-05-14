c309818dee65f699b31b987068c6c58ac11b63a3
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-13 20:11:41 -0600

    Se agregó la estructura usuario

---
+++ usuarios/usuarios.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+typedef struct
+{
+    char IDU;
+    char nombreU[30];
+    char apellido[30];
+    char email[100];
+    char password[20];
+}usuario[MAX];
+
+usuario usuarios[MAX];
