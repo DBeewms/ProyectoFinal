11fcae6a9cc40e6c973ab78f8a0a789ea8f6a85d
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-13 20:04:51 -0600

    Se agrega la estructura productos

---
+++ productos/productos.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+
+typedef struct
+{
+    char IDP[10];
+    char nombreP[30];
+    char descripcion[100];
+    float precio;
+}producto[MAX];
+
+producto productos[MAX];
