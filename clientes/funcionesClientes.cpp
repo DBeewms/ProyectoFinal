7a0627b33c8cdcdeaba50f56fb287dc81ed7b5b9
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-13 19:55:54 -0600

    Se agregaron las funciones al archivos funconesClientes.cpp

---
+++ clientes/funcionesClientes.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "clientes.h"
+
+using namespace std;
+// Se pone la posicion de donde se iniciara
+int UltmoCl=0;
+// se agregan fuciones
+void agregarCliente();
+void editarCliente();
+void eliminarCliente();
+int existeRegistroCliente(int codigo);
+void menuCliente();
+
+// Se definen las funciones
+
+void agregar(){
+
+}
