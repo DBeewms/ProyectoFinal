0d8b816c78d0076b9377618b37ef063a4210d145
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-13 20:17:39 -0600

    se agregaron las funciones al archivos funcionesUsuario.cpp

---
+++ usuarios/funcionesUsuarios.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "usuarios.h"
+
+using namespace std;
+
+void agregarUsuario();
+void editarUsuario();
+void eliminarUsuario();
+int existeRegistroUsuario(char IDU[]);
+void menuUsuarios();
