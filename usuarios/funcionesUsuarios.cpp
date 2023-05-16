69abec993509a1ab775289c32a5da08c27037ebb
shurkyt <leandro.nunez15935@est.uca.edu.ni>
2023-05-15 20:00:47 -0600

    Se agregaron funciones al archivo funcionesUsuarios.cpp

--- usuarios/funcionesUsuarios.cpp
+++ usuarios/funcionesUsuarios.cpp
@@ -3,9 +3,79 @@
 #include "usuarios.h"
 
 using namespace std;
+int lastRegU = 0;
 
-void agregarUsuario();
+// create
+void agregarUsuario(usuario Usuario);
+/*Read*/
+void mostrarUsuario(int pos);
+void mostrarUsuarios();
+int buscarUsuario(char IDU[]);
+void inicialUsuario(int pos);
+usuario getUsuarios(int pos);
+/*Update*/
 void editarUsuario();
-void eliminarUsuario();
-int existeRegistroUsuario(char IDU[]);
+/*Delete*/
+void eliminarUsuario(int pos);
+/*Menu*/
 void menuUsuarios();
+void menuCompleto();
+/*Archivos*/
+FILE *resgistroUsuario;
+void guardarUsuario();
+void leerUsuario();
+void calcularReg(FILE *archivo);
+
+
+/*Se definen las funciones*/
+
+void agregarUsuario(usuario Usuario)
+{
+    usuarios[lastRegU] = Usuario;
+    lastRegU++;
+}   
+
+
+void mostrarUsuario(int pos)
+{
+    cout << "ID:" << usuarios[pos].IDU << endl;
+    cout << "NOMBRE DE USUARIO:" << usuarios[pos].nombreU << endl;
+    cout << "EMAIL:" << usuarios[pos].email << endl;
+    cout << "CONTRASEÑA:" <<usuarios[pos].password << endl;
+
+}
+
+usuario getUsuarios(int pos)
+{
+    return usuarios[pos];
+}
+
+int buscarUsuario(char IDU[])
+{
+    int posicion = 0;
+    for(int i = 0; i < lastRegU; i++)
+    {
+        if (strcmp(IDU, usuarios[i].IDU) == 0)
+        {
+            posicion = i;
+        }
+    }
+    return posicion;
+}
+
+void mostrarUsuarios()
+{
+    system("cls||clear");
+    if (lastRegU == 0)
+    {
+        cout << "No hay usuarios registrados" << endl;
+        return;
+    }
+
+    for(int i = 0; i < lastRegU; i++)
+    {
+       cout<< "=========================\n";
+       mostrarUsuario(i); 
+    }
+    cout << "Ultimo usuario...." << endl;
+}
