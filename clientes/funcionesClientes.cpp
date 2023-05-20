a15eeb6f810383918540d7ca07d7206d35ce9913
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-05-20 10:02:26 -0600

    Se agregaron las funciones de borrar cliente, cliente incial, el menu de cliente

--- clientes/funcionesClientes.cpp
+++ clientes/funcionesClientes.cpp
@@ -44,7 +44,6 @@ void MostrarCliente(int pos)
     cout << "ID: " << Clien[pos].ID << endl;
     cout << "NOMBRE: " << Clien[pos].nombre << endl;
     cout << "APELLIDO: " << Clien[pos].apellido << endl;
-    cout << "DIRECCION: " << Clien[pos].direccion << endl;
 }
 
 cliente getClientes(int pos)
@@ -65,16 +64,145 @@ int BuscarCliente(char id[])
     return posicion;
 }
 
-void MostrarClientes(){
+void MostrarClientes()
+{
     system("cls||clear");
-    if(lastReg==0){
+    if (lastReg == 0)
+    {
         cout << "No hay clientes registrados " << endl;
         return;
     }
-    for(int i = 0; i < lastReg; i++)
+    for (int i = 0; i < lastReg; i++)
     {
         cout << "======================\n";
         MostrarCliente(i);
     }
-    cout << "Ultimo cliente..... " <<endl;
+    cout << "Ultimo cliente..... " << endl;
+}
+
+void editarCliente(cliente Clientes, int pos)
+{
+    Clien[pos];
+}
+
+void borrarCliente(int pos)
+{
+    if (pos == lastReg)
+    {
+        cout << "No hay registro " << endl;
+        return;
+    }
+    for (int i = pos; i < lastReg - 1; i++)
+    {
+        Clien[i] = Clien[i + 1];
+    }
+    lastReg--;
+    ClienteInicial(lastReg);
+}
+
+void ClienteInicial(int pos)
+{
+    strcpy(Clien[pos].ID, "");
+    strcpy(Clien[pos].nombre, "");
+    strcpy(Clien[pos].apellido, "");
+}
+
+int menuC()
+{
+    int op;
+
+    cout << "-----------------Bienvenido al menu de MCSerigraph-------------- " << endl;
+    cout << "Seleccione una de las opciones " << endl;
+    cout << "Ver la cantidad de clientes registrados " << lastReg << endl;
+    cout << "1. Agregar cliente " << endl;
+    cout << "2. Editar cliete " << endl;
+    cout << "3. Eliminar cliente " << endl;
+    cout << "4. Buscar cliente " << endl;
+    cout << "5. Mostrar todos los clientes " << endl;
+    cout << "6. Salir " << endl;
+    cout << " Digite la opcion " << endl;
+    cin >> op;
+    return op;
+}
+
+void MenuComplete()
+{
+    int op, pos, resp;
+    char ID[14];
+    cliente Clientes;
+    leerCliente();
+
+    do
+    {
+        system("cls||clear");
+
+        op = menuC();
+        switch (op)
+        {
+        case 1:
+            system("cls||clear");
+            cout << " ID: " << endl;
+            cout << " Nombre: " << endl;
+            cout << " Apellido: " << endl;
+            scanf(" %[^\n]", Clientes.ID);
+            scanf(" %[^\n]", Clientes.nombre);
+            scanf(" %[^\n]", Clientes.apellido);
+            agregarCliente(Clientes);
+            system("pause");
+
+            break;
+        case 2:
+            system("cls||clear");
+            cout << "Escribe el ID a buscar: " << endl;
+            scanf(" %[^\n]", Clientes.ID);
+            pos = BuscarCliente(ID);
+            MostrarCliente(pos);
+            cout << " Datos a editar " << endl;
+            cout << " ID: " << endl;
+            scanf(" %[^\n]", Clientes.ID);
+            cout << " Nombre: " << endl;
+            scanf(" %[^\n]", Clientes.nombre);
+            cout << " Apellido: " << endl;
+            scanf(" %[^\n]", Clientes.apellido);
+            editarCliente(Clientes, pos);
+            cout << " Actualizando registro.......... " << endl;
+            system("pause");
+            break;
+        case 3:
+            if (lastReg == 0)
+            {
+                cout << " No hay nada que eliminar " << endl;
+                break;
+            }
+            cout << "Escribe el ID del cliente: " << endl;
+            cin >> ID;
+            pos = BuscarCliente(ID);
+            Clientes = getClientes(pos);
+            cout << "¿Realmente quiere solicitar elimimar a este cliente: " << Clientes.nombre << "" << Clientes.apellido << " ? " << endl;
+            cout << "Escribe 1 para acceder y 2 para negar: " << endl;
+            cin >> resp;
+            if (resp == 1)
+            {
+                borrarCliente(pos);
+                cout << " Registro Eliminado " << endl;
+            }
+            else
+            {
+                cout << " Operacion cancelada " << endl;
+            }
+            system("pause");
+            break;
+        case 4:
+            system("cls||clear");
+            cout << " Escribe el ID a buscar: " << endl;
+            scanf(" %[^\n]", Clientes.ID);
+            pos = BuscarCliente(ID);
+            MostrarCliente(pos);
+            system("pause");
+            break;
+
+        default:
+            break;
+        }
+    } while (op != 6);
 }
