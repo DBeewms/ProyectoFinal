c62b50dca4f4cbc0c693fa9a04ee9b47eb830a40
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-21 10:17:06 -0600

    Se desarrollan las funciones encabezadoFactura, pieDeFactura, agregarFactura, getFacturas, buscandoCliente y buscandoProducto

--- funcionesFacturas.cpp
+++ funcionesFacturas.cpp
@@ -4,22 +4,98 @@
 #include <string.h>
 #include "clientes/funcionesClientes.cpp"
 #include "productos/funcionesProductos.cpp"
+#include "facturas.h"
 
 using namespace std;
 
-
 int lastFactura = 0;
 
-void EncabezadoFactura();
-void agregarFactura();
+void encabezadoFactura();
+void pieDeFactura();
+
+void agregarFactura(factura facturas);
+
+factura getFacturas(int pos);
+
+int buscandoCliente(int position11);
+int buscandoProducto(int position22);
+
 void MostrarFactura();
 void MostrarFacturas();
 void editarFactura();
 void ExisteFactura();
 void EliminarFacturas();
 
-
 void menuPrincipal();
+
+//agregar datos
+//hacer que # de factura funcione
+void encabezadoFactura(){
+    cout << "===================MCSerigraph===================" << endl;
+    cout << "Direccion:" << endl;
+    cout << "Telefono:" << endl;
+    cout << "Factura #" << endl;
+    cout << "=================================================" << endl;
+}
+
+void pieDeFactura(){
+    cout << "=================================================" << endl;
+    cout << "Gracias por su compra";
+    cout << "=================================================" << endl;
+}
+
+void agregarFactura(factura facturas){
+    recibo[lastFactura] = facturas;
+    lastFactura++;
+}
+
+factura getFacturas(int pos){
+    return recibo[pos];
+}
+
+int buscandoCliente(int position11){
+    leerCliente();
+
+    char IDpos1[14];
+    cliente Clientes;
+
+    cout << " Escribe el ID del cliente para agregar: " << endl;
+    scanf(" %[^\n]", Clientes.ID);
+    position11 = BuscarCliente(IDpos1);
+    cout << endl;
+    return position11;
+}
+
+int buscandoProducto(int position22){
+    leerProducto();
+
+    char IDpos2[14];
+    producto productos;
+
+
+    cout << " Escribe el ID producto para agregar: " << endl;
+    scanf(" %[^\n]", productos.IDP);
+    position22 = BuscarProducto(IDpos2);
+    return position22;
+}
+
+void MostrarFactura(){
+    int position11; 
+    int position22;
+
+    buscandoCliente(position11);
+    buscandoProducto(position22);
+
+    position11 = buscandoCliente(position11);
+    position22 = buscandoProducto(position22);
+
+    MostrarCliente(position11);
+    MostrarProducto(position22);
+}
+
+
+
+
 
 void menuPrincipal(){
     int option;
