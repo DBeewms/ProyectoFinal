6f2637fb5b777dca87260acc6d8ad11e66dbf8be
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-21 08:35:42 -0600

    Se agrega la funcion menuPrincipal y se desarrolla para tener un menu de opciones central

--- funcionesFacturas.cpp
+++ funcionesFacturas.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include "funcionesClientes.cpp"
-#include "funcionesProductos.cpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "clientes/funcionesClientes.cpp"
+#include "productos/funcionesProductos.cpp"
 
 using namespace std;
 
@@ -14,3 +17,55 @@ void MostrarFacturas();
 void editarFactura();
 void ExisteFactura();
 void EliminarFacturas();
+
+
+void menuPrincipal();
+
+void menuPrincipal(){
+    int option;
+    do
+    {
+        system("cls||clear");
+
+        cout << "-----------------Bienvenido al menu de MCSerigraph--------------" << endl;
+        cout << "Seleccione una de las opciones: " << endl;
+        cout << "1. Seccion de clientes" << endl;
+        cout << "2. Seccion de productos" << endl;
+        cout << "3. Seccion de usuarios" << endl;
+        cout << "4. Seccion de facturas" << endl;
+        cout << "7. Salir" << endl;
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+            system("cls||clear");
+            cout << "Entrando a la seccion de clientes...";
+            MenuCompleteClient();
+            system("pause");
+            break;
+        case 2:
+            system("cls||clear");
+            cout << "Entrando a la seccion de productos...";
+            MenuCompleteProduct();
+            system("pause");
+            break;
+        case 3:
+            system("cls||clear");
+            cout << "Entrando a la seccion de usuarios...";
+            system("pause");
+            break;    
+        case 4:
+            system("cls||clear");
+            cout << "Entrando a la seccion de facturas...";
+            system("pause");
+            break;    
+        default:
+            cout << "Opcion invalida, selecciona una opcion del menu...";
+            system("pause");
+            break;
+        }
+    } while (option != 7);
+    
+    
+}
