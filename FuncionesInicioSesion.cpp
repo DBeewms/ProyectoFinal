e271b09d709df49f99179fa52ea031a38db5d66c
Enrique990 <enrique.taleno16055@est.uca.edu.ni>
2023-05-27 17:42:14 -0600

    se corrigieron algunas imperfecciones

--- FuncionesInicioSesion.cpp
+++ FuncionesInicioSesion.cpp
@@ -32,7 +32,7 @@ void menuInicial(){
 }
 
 void menuAdmin(){
-    int option;
+    int optionA;
     do
     {
         system("cls||clear");
@@ -44,37 +44,40 @@ void menuAdmin(){
         cout << "3. Seccion de usuarios" << endl;
         cout << "4. Seccion de facturas" << endl;
         cout << "7. Salir" << endl;
-        cin >> option;
+        cin >> optionA;
 
-        switch (option)
+        switch (optionA)
         {
         case 1:
-            system("cls||clear");
+            system("cls");
             cout << "Entrando a la seccion de clientes...";
             MenuCompleteClient();
             system("pause");
             break;
         case 2:
-            system("cls||clear");
+            system("cls");
             cout << "Entrando a la seccion de productos...";
             MenuCompleteProduct();
             system("pause");
             break;
         case 3:
-            system("cls||clear");
+            system("cls");
             cout << "Entrando a la seccion de usuarios...";
             MenuCompleteUsuario();
             system("pause");
             break;    
         case 4:
-            system("cls||clear");
+            system("cls");
             cout << "Entrando a la seccion de facturas...";
             system("pause");
+            break;
+        case 5:
+            cout << "Saliendo....... " << endl;
             break;    
         default:
             cout << "Opcion invalida, selecciona una opcion del menu...";
             system("pause");
             break;
         }
-    } while (option != 7);
+    } while (optionA != 5);
 }
