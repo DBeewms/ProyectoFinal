6ee780d867d88a4bf275350d5e72de9ae8181d92
Enrique990 <enrique.taleno16055@est.uca.edu.ni>
2023-05-27 17:17:02 -0600

    se simplifico el menu anteerior

--- principal.cpp
+++ principal.cpp
@@ -2,58 +2,14 @@
 #include "clientes/funcionesClientes.cpp"
 #include "productos/funcionesProductos.cpp"
 #include "usuarios/funcionesUsuarios.cpp" 
-
+#include "FuncionesInicioSesion.cpp"
 
 using namespace std;
 
+void menuAdmin();
 
 int main()
 {
-    int option;
-    do
-    {
-        system("cls||clear");
-
-        cout << "-----------------Bienvenido al menu de MCSerigraph--------------" << endl;
-        cout << "Seleccione una de las opciones: " << endl;
-        cout << "1. Seccion de clientes" << endl;
-        cout << "2. Seccion de productos" << endl;
-        cout << "3. Seccion de usuarios" << endl;
-        cout << "4. Seccion de facturas" << endl;
-        cout << "7. Salir" << endl;
-        cin >> option;
-
-        switch (option)
-        {
-        case 1:
-            system("cls||clear");
-            cout << "Entrando a la seccion de clientes...";
-            MenuCompleteClient();
-            system("pause");
-            break;
-        case 2:
-            system("cls||clear");
-            cout << "Entrando a la seccion de productos...";
-            MenuCompleteProduct();
-            system("pause");
-            break;
-        case 3:
-            system("cls||clear");
-            cout << "Entrando a la seccion de usuarios...";
-            MenuCompleteUsuario();
-            system("pause");
-            break;    
-        case 4:
-            system("cls||clear");
-            cout << "Entrando a la seccion de facturas...";
-            system("pause");
-            break;    
-        default:
-            cout << "Opcion invalida, selecciona una opcion del menu...";
-            system("pause");
-            break;
-        }
-    } while (option != 7);
-
+    menuAdmin();
     return 0;
 }
