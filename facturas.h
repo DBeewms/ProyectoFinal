9dd8db43bf586288b41a0e09bced10c6da4e2197
DBeewms <diego.mora16063@est.uca.edu.ni>
2023-05-30 20:54:33 -0600

    Se agregaron variables a las estructuras de facturas.h

--- facturas.h
+++ facturas.h
@@ -4,7 +4,22 @@
 
 typedef struct
 {
-    char numFactura[10];
+    int numFactura;
+    char fecha[10];
+    char nombreCliente[60];
+    char observacion[100];
 } factura;
 
+typedef struct
+{
+    int idDetalle;
+    int numFac;
+    char nomProducto[30];
+    float precioP;
+    int cantidad; 
+} detFac; //detalles factura
+
 factura recibo[1000];
+
+detFac detalles[10000];
+
