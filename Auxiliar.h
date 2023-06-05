437329deb6c5a71fd19a1fbf702cbb58536384ca
masterc18 <carlos.talavera16038@est.uca.edu.ni>
2023-06-05 16:50:44 -0600

    Se mejoro el archivo de auxiliar.h

--- Auxiliar.h
+++ Auxiliar.h
@@ -1,6 +1,6 @@
 #ifdef _WIN32 // Para Windows
 #include <windows.h>
-void gotoxy(int x, int y) {
+void gotoxy4(int x, int y) {
     COORD coord = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
